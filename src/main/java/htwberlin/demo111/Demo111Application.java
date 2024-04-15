b5f2531bd6afab9e4f5e588352356848c4414471
Calvin Vi <Calvin.Vi@Student.HTW-Berlin.de>
2024-04-15 16:33:50 +0200

    test 111

---
+++ src/main/java/htwberlin/demo111/Demo111Application.java
@@ -0,0 +1,13 @@
+package htwberlin.demo111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo111Application.class, args);
+    }
+
+}
